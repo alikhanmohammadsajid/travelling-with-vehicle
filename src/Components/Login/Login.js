3704b6991bdee39368b161458e6f7f1b66d6b81b
alikhanmohammadsajid <alikhanmohammadsajid@gmail.com>
2021-03-21 17:26:35 +0600

    lst commit

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -32,10 +32,56 @@ const Login = () => {
         });
     }
 
+    const handleBlur = (e) => {
+        let isFormValid = true
+        if (e.target.name === "email") {
+            isFormValid = /\S+@\S+\.\S+/.test(e.target.value)
+
+        }
+
+        if (e.target.name === "password") {
+            const isPasswordValid = e.target.value.length > 6;
+            const passwordNum = /\d{1}/.test(e.target.value)
+            isFormValid = (isPasswordValid && passwordNum);
+        }
+        if (isFormValid) {
+            const newUserInfo = { ...loggedInUser }
+            newUserInfo[e.target.name] = [e.target.value]
+            setLoggedInUser(newUserInfo)
+        }
+    }
+    const handleSubmit = (e) => {
+        
+        if (loggedInUser.email && loggedInUser.password) {
+            firebase.auth().createUserWithEmailAndPassword(loggedInUser.email, loggedInUser.password)
+                
+                .catch((error) => {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    console.log(errorCode, errorMessage);
+                });
+        }
+        e.preventDefault()
+    }
+
     return (
-        <div  style={{textAlign:"center"}}>
-            
-            <button style={{borderRadius:"10px"}} className="btn-warning" onClick={handleGoogleSignIn}>Google Sign In</button>
+        <div style={{ textAlign: "center" }}>
+
+            <button style={{ borderRadius: "10px" }} className="btn-warning" onClick={handleGoogleSignIn}>Google Sign In</button>
+
+            <br />
+            <br />
+            <form onsubmit={handleSubmit}>
+
+                <input name="name" style={{ borderRadius: "5px", margin: "10px" }} onBlur={handleBlur} type="text" placeholder="name" />
+                <br />
+                <input name="email" style={{ borderRadius: "10px", margin: "10px" }} onBlur={handleBlur} type="text" placeholder="email" required />
+                <br />
+                <input style={{ borderRadius: "10px", margin: "10px" }} onBlur={handleBlur} type="password" name="password" placeholder="password" required />
+                <br />
+                <input style={{ borderRadius: "10px", margin: "10px" }} onClick={handleSubmit} type="submit" value="submit" />
+
+            </form>
         </div>
     );
 };
